a5953cba769312ff02c2a5816ba1ef00947bcded
Andre Renaud <arenaud@designa-electronics.com>
2018-04-23 08:26:04 +1200

    Commit of 0.6.0

--- Makefile
+++ Makefile
@@ -1,33 +1,43 @@
+#
+# Makefile for X-on-screen display
+#
+
 LIBRARY_VERSION=0
+VERSION=0.6.0
+
 PREFIX=/usr/local
+EXEC_PREFIX=$(PREFIX)
+BINDIR=$(EXEC_PREFIX)/bin
+LIBDIR=$(EXEC_PREFIX)/lib
+MANDIR=$(PREFIX)/man
+INCLUDEDIR=$(PREFIX)/include
+XMMS_PLUGINDIR=$(HOME)/.xmms
+
 CC=gcc
-VERSION=0.5.0
+INSTALL=/usr/bin/install -c
+INSTALL_DATA=${INSTALL} -m 644
+
 CFLAGS=-g -Wall -pipe -I. -DXOSD_VERSION=\"$(VERSION)\" -I/usr/X11R6/include -fPIC
-LFLAGS=-L. -lxosd -fpic
+LDFLAGS=-L. -lxosd -fpic
 SOLFLAGS=-L/usr/X11R6/lib -lX11 -lXext -lpthread -lXt -fPIC
 SOURCES=AUTHORS CHANGELOG README LICENSE Makefile testprog.c xosd.c xosd.h \
-	xmms_osd.c osd_cat.c xosd.3
+	xmms_osd.c osd_cat.c xosd.3 osd_cat.1
 
-default: testprog libxmms_osd.so osd_cat
+all: testprog libxmms_osd.so osd_cat
 
-%.o: %.c
-	$(CC) -c -o $@ $< $(CFLAGS)
-	
 libxosd.so: xosd.o
 	$(CC) -shared -o $@ xosd.o $(SOLFLAGS) \
 		-Wl,-soname,libxosd.so.$(LIBRARY_VERSION)
-	
-testprog: testprog.o libxosd.so
-	$(CC) -o $@ testprog.o $(LFLAGS)
 
 xmms_osd.o: xmms_osd.c
 	$(CC) -c -o $@ xmms_osd.c `gtk-config --cflags` `xmms-config --cflags` $(CFLAGS)
 
-libxmms_osd.so: xmms_osd.o 
-	$(CC) -shared -o $@ $+ `gtk-config --libs` $(LFLAGS)
+libxmms_osd.so: xmms_osd.o
+	$(CC) -shared -o $@ $+ `gtk-config --libs` $(LDFLAGS)
+
+testprog: testprog.o libxosd.so
 
 osd_cat: libxosd.so osd_cat.o
-	$(CC) -o $@ osd_cat.o $(LFLAGS)
 
 tar: xosd-$(VERSION).tar.gz
 
@@ -36,18 +46,21 @@ xosd-$(VERSION).tar.gz:
 	tar cfz $@ $(patsubst %, xosd-$(VERSION)/%, $(SOURCES))
 	rm xosd-$(VERSION)
 
-install: default
-	cp -a libxosd.so $(PREFIX)/lib/libxosd.so.$(LIBRARY_VERSION)
+install: all
+	$(INSTALL) libxosd.so $(LIBDIR)/libxosd.so.$(LIBRARY_VERSION)
 	rm $(PREFIX)/lib/libxosd.so
 	ln -s $(PREFIX)/lib/libxosd.so.$(LIBRARY_VERSION) $(PREFIX)/lib/libxosd.so
-	mkdir -p $(HOME)/.xmms/Plugins/Visualization
-	rm -f $(HOME)/.xmms/Plugins/Visualization/libxmms_osd.so
+	mkdir -p $(XMMS_PLUGINDIR)/Plugins/Visualization
+	rm -f $(XMMS_PLUGINDIR)/Plugins/Visualization/libxmms_osd.so
 	cp -a libxmms_osd.so $(HOME)/.xmms/Plugins/Visualization
-	cp -a osd_cat $(PREFIX)/bin
-	mkdir -p $(PREFIX)/man/man3
-	cp -a xosd.3 $(PREFIX)/man/man3
+	$(INSTALL) osd_cat $(BINDIR)
+	mkdir -p $(MANDIR)/man1 $(MANDIR)/man3
+	$(INSTALL_DATA) osc_cat.1 $(MANDIR)/man1/
+	$(INSTALL_DATA) xosd.3 $(MANDIR)/man3/
 
 clean:
 	rm -f *~ *.o xosd testprog libxosd.so libxmms_osd.so libxosd.a osd_cat
 	rm -f xosd-$(VERSION).tar.gz
-	
+
+.PHONY: all tar clean install
+# vim: noexpandtab
--- osd_cat.c
+++ osd_cat.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <xosd.h>
+#include <unistd.h>
+#include <getopt.h>
+
+static struct option long_options[] = {
+    {"font",   1, NULL, 'f'},
+    {"color",  1, NULL, 'c'},
+    {"delay",  1, NULL, 'd'},
+    {"offset", 1, NULL, 'o'},
+    {"top",    0, NULL, 't'},
+    {"bottom", 0, NULL, 'b'},
+    {"help",   0, NULL, 'h'},
+    {NULL,     0, NULL, 0}
+};
 
 int main (int argc, char *argv[])
    {
@@ -9,19 +22,66 @@ int main (int argc, char *argv[])
    xosd *osd;
    char buffer [1024];
    char *newline;
+
+   char *font = "fixed";
+   char *color = "red";
+   int delay = 5;
+   xosd_pos pos = XOSD_top;
+   int offset = 0;
+
+   while (1)
+   {
+       int option_index = 0;
+       int c = getopt_long (argc, argv, "f:c:d:o:tbh", long_options, &option_index);
+       if (c == -1) break;
+       switch (c)
+       {
+	   case 'f':
+	       font = optarg;
+	       break;
+	   case 'c':
+	       color = optarg;
+	       break;
+	   case 'd':
+	       delay = atoi(optarg);
+	       break;
+	   case 'o':
+	       offset = atoi(optarg);
+	       break;
+	   case 't':
+	       pos = XOSD_top;
+	       break;
+	   case 'b':
+	       pos = XOSD_bottom;
+	       break;
+	   case '?':
+	   case 'h':
+	       fprintf (stderr, "Usage: %s [OPTION] [FILE]...\n", argv[0]);
+	       fprintf (stderr, "Display FILE, or standard input, on top of display.\n\n");
+	       fprintf (stderr, "  -t, --top           Display at top of screen\n");
+	       fprintf (stderr, "  -b, --bottom        Display at bottom of screen\n");
+	       fprintf (stderr, "  -f, --font=FONT     Use font\n");
+	       fprintf (stderr, "  -c, --color=COLOR   Use color\n");
+	       fprintf (stderr, "  -d, --delay=TIME    Show for specified time\n");
+	       fprintf (stderr, "  -o, --offset=OFFSET Display Offset\n");
+	       fprintf (stderr, "  -h, --help          Show this help\n");
+	       fprintf (stderr, "\nWith no FILE, or when FILE is -, read standard input.\n");
+	       return EXIT_SUCCESS;
+       }
+   }
    
-   if (argc != 1)
+   if ((optind < argc) && strncmp(argv[optind], "-", 2))
       {
-      if ((fp = fopen (argv[1], "r")) == NULL)
+      if ((fp = fopen (argv[optind], "r")) == NULL)
 	 {
-	 fprintf (stderr, "Unable to open: %s\n", argv[1]);
+	 fprintf (stderr, "Unable to open: %s\n", argv[optind]);
 	 return EXIT_FAILURE;
 	 }
       }
    else
       fp = stdin;
    
-   osd = xosd_init ("fixed", "red", 5, XOSD_top, 0);
+   osd = xosd_init (font, color, delay, pos, offset);
    if (!osd)
       {
       fprintf (stderr, "Error initializing osd\n");
@@ -44,3 +104,5 @@ int main (int argc, char *argv[])
    
    return EXIT_SUCCESS;
    }
+/* vim: ai si sw=4
+ */
